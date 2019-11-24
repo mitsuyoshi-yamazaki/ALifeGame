a25627bbda4b7dd3f54b72dceaf23acfa44dab19
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-11-24 19:39:17 +0900

    ノードをタップするとカメラが追従する

--- ALifeGame/GameScene.swift
+++ ALifeGame/GameScene.swift
@@ -11,19 +11,17 @@ import GameplayKit
 
 final class GameScene: SKScene {
   
-  private var crosshairNode: SKNode! {
-    return childNode(withName: "//crosshair")
+  private var cameraNode: SKNode! {
+    return childNode(withName: "//camera")
   }
 
   override func didMove(to view: SKView) {
   }
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let touchLocation = touches.first?.location(in: self) else {
-      return
+    if let node = tappedNode(of: touches) {
+      nodeDidTap(node)
     }
-    let action = SKAction.move(to: touchLocation, duration:0.4)
-    crosshairNode.run(action)
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,9 +32,37 @@ final class GameScene: SKScene {
   
   override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
   }
-  
-  
+
   override func update(_ currentTime: TimeInterval) {
     // Called before each frame is rendered
+  }
+}
+
+extension GameScene {
+  enum Action: String {
+    case readDocumentation = "book_shelf"
+    case enterALifeWorld   = "display"
+    case checkNotification = "notification" // TODO: implement
+    case seeClock          = "clock"
+    case seePicture        = "picture"
+  }
+
+  private func tappedNode(of touches: Set<UITouch>) -> SKNode? {
+    guard let touchLocation = touches.first?.location(in: self) else {
+      return nil
+    }
+    return atPoint(touchLocation)
+  }
+
+  private func nodeDidTap(_ node: SKNode) {
+    guard let action = Action.init(rawValue: node.name ?? "") else {
+      return
+    }
+    execute(with: action, node: node)
+  }
+
+  private func execute(with action: Action, node: SKNode) {
+    let action = SKAction.move(to: node.position, duration:0.4)
+    cameraNode.run(action)
   }
 }
